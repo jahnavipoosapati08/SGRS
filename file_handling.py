ea6fbddbbe41de7e42cc08c48fbb82fc3554c1ee
jahnavipoosapati08 <jahnavipoosapati@gmail.com>
2026-04-16 08:33:37 +0530

    Delete file_handling.py

--- file_handling.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("grievances.csv", "w", newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["ID", "Name"])
-    writer.writerow([1, "Jahnavi"])
-
-with open("grievances.csv", "r") as file:
-    print(file.read())
