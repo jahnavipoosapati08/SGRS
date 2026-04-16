e8e1e010a9cdb5274fade1e0b7c17aabc6692e79
jahnavipoosapati08 <jahnavipoosapati@gmail.com>
2026-04-16 08:32:03 +0530

    Delete analytics.py

--- analytics.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-categories = ["Academic", "Hostel", "Transport"]
-values = [10, 5, 7]
-
-plt.bar(categories, values)
-plt.title("Grievance Categories")
-plt.show()
